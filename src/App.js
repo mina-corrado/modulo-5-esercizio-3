124b1ed85be504419e735dcf3e4f51b7552d9b54
mina-corrado <mina.corrado@hotmail.it>
2023-02-19 21:15:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { BookList } from './BookList';
+import { MyBadge } from './MyBadge';
+import scifiBooks from './scifi.json';
+import { SingleBook } from './SingleBook';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <MyBadge text="Testo Badge" color="primary"></MyBadge>
+      <SingleBook book={scifiBooks[0]}></SingleBook>
+      <BookList books={scifiBooks}></BookList>
     </div>
   );
 }
---
+++ src/SingleBook.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+export class SingleBook extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            selected: false,
+        }
+    }
+
+    render() {
+        const book = this.props.book;
+        const state = this.state;
+        
+        const myStyle = state.selected===true ? { borderWidth: 4, borderColor: 'red', borderStyle: 'solid' } : null;
+
+        return (
+            <Card style={myStyle} onClick={()=>{this.setState({selected: !state.selected})}}>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Text>
+                        Asin {book.asin} - Prezzo {book.price}€
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
